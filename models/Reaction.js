dd044da74cab9bfdcfd3a318e2ade8271e3a0770
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-07 16:06:57 +0200

    Set Reaction model

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionID: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => date.toLocaleDateString('de-DE'),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+    },
+);
+
+module.exports = reactionSchema;
