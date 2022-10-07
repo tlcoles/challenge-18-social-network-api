763795756e80d153f3d2fd96e411d31b96fbcf61
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-07 16:58:48 +0200

    Create api user route

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController.js');
+
+// /api/users
+router.router('/')
+    .get(getUsers)
+    .post(createUser);
+
+// /api/users/:userID
+router.route('/userID')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
