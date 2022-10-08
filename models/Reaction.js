3b59ce75e4afb5031fd548e273ecfc67085f8740
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-08 09:35:52 +0200

    Change ID to Id

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,7 +2,7 @@ const { Schema, Types } = require('mongoose');
 
 const reactionSchema = new Schema(
     {
-        reactionID: {
+        reactionId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId(),
         },
