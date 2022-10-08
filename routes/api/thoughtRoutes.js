3b203b0fb12559acd42111831e6c9b8165f7606a
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-08 09:37:13 +0200

    Set reactionSchema

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController.js');
+
+// get all thoughts or create a new one
+router.route('/')
+    .get(getThoughts)
+    .post(createThought);
+
+// find a single thought or delete it via :thoughtId
+router.route('/:thoughtId')
+    .get(getSingleThought)
+    .delete(deleteThought)
+    .put(updateThought);
+
+// post or delete reactions to thoughts 
+router.route('//:thoughtId/reactions')
+    .post(createReaction)
+    .delete(deleteReaction);
