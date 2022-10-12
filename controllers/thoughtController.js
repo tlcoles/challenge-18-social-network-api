1397899c2a462251310d91eabd1cf7c6e1e10df5
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-12 19:44:08 +0200

    Get single thought by ID

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,4 +7,15 @@ module.exports = {
             .then((thoughts) => res.json(thoughts))
             .catch((error) => res.status(500).json(error));
     },
+    // Get single thought by ID
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+        .select('-__v')
+        .then((thought) => 
+        !thought
+            ? res.status(404).json({ message: 'No thought found matching that ID!'})
+            : res.json(thought)
+            )
+            .catch((error) => res.status(500).json(error));
+    },
 }
